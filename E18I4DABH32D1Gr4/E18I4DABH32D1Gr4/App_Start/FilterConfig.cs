95e45214263888832d0fda4171dd36029ef84e32
no u <ahs3005@hotmail.com>
2018-11-16 09:58:30 +0100

    Created Project for HandIn 3.2

---
+++ E18I4DABH32D1Gr4/E18I4DABH32D1Gr4/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace E18I4DABH32D1Gr4
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ E18I4DABH32D1Gr4/E18I4DABH32D1Gr4/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace E18I4DABH32D1Gr4.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
