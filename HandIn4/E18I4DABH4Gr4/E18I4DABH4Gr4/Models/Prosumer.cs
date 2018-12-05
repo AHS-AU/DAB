b1a6f1c020f94f291b9dbfbc5b776f1ce7ea80d5
Jonas <jjy1993@gmail.com>
2018-12-05 11:34:47 +0100

    Diagram, and models added

---
+++ HandIn4/E18I4DABH4Gr4/E18I4DABH4Gr4/Models/Prosumer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E18I4DABH4Gr4.Models
+{
+    public class Prosumer
+    {
+    }
+}
