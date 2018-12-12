e9d691c944a0c2eea41f1679f36e3bc9b26d218c
Unknown <ahs3005@hotmail.com>
2018-12-12 19:07:00 +0100

    First API Template Created

---
+++ HandIn4/Simulation/HandIn4_Simulation/HandIn4_Simulation/Program.cs
@@ -0,0 +1,27 @@
+﻿using HandIn4_Simulation.Controllers;
+using HandIn4_Simulation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandIn4_Simulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TradersRepoController mTradersRepoController = new TradersRepoController();
+
+            List<Trader> GetAllTraders = mTradersRepoController.GetTradersAsync().Result.ToList();
+
+            foreach (var v in GetAllTraders)
+            {
+                Console.WriteLine(v);
+            }
+
+
+        }
+    }
+}
